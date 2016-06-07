faf6d5084694a1e8550337b932001d27ae41862b
Dennis Cwik <dennis.cwik@gmail.com>
2016-06-06 20:30:04 -0700

    Restructure code, add error codes
    Rather than have 1 monolithic method, I've added in methods which
    return error codes.

--- src/temperature/temperature.ino
+++ src/temperature/temperature.ino
@@ -4,18 +4,35 @@
  * Date: June 4, 2016
  *
  * Logic for reading temperature using the OneWire v2.3.2 library,
- * (commit 57c18c6de80c13429275f70875c7c341f1719201), with a
- * DS18B20 sensor connected to pin 10.
+ * (commit 57c18c6de80c13429275f70875c7c341f1719201), with an
+ * externally powered (ie, not in parasitic mode) DS18B20 sensor
+ * connected to pin 10, as well as a 4.7K Ohm resistor between 5V
+ * and pin 10.
  *
  * This example code is in the public domain.
  */
+ 
+ // TODOs
+ // -ensure 12 bit resolution
+ // -figure out "read time slots"
+ // -ensure single device
+ // -ensure device has an external power supply
 
 #include <OneWire.h>
 
 // DEBUG can be toggled to make serial communication less noisy
 #define DEBUG true
 
+// pin assignments
 #define ONE_WIRE_PIN 10
+
+// error codes
+#define NO_ERROR                      0
+#define ERROR_OWB_NO_DEVICES          1
+#define ERROR_OWB_TOO_MANY_DEVICES    2
+#define ERROR_OWB_ADDRESS_CRC         3
+#define ERROR_OWB_NOT_28_FAMILY       4
+#define ERROR_OWB_SCRATCHPAD_READ_CRC 5
 
 // create the 1-wire bus
 OneWire ds(ONE_WIRE_PIN);
@@ -27,74 +44,112 @@ void setup(void)
 
 void loop(void)
 {
-  // re-usable iterator
-  byte i;
-  
-  // TODO figure out the point of this!
-  byte present = 0;
-  
- // TODO figure out why this is 12!
+  // TODO figure out why this is 12!
   byte data[12];
   
   // 8 byte address for devices on the 1-wire bus
   byte addr[8];
   
-  boolean isNegative = false;
-  int whole = 0;
-  int fract = 0;
+  // the 2-byte temperature reading from the sensor
   int sensorReading = 0;
   
-  // Search for the next device on the 1-wire bus. If found, its
+  int result;
+  
+  result = findSingleDevice(addr);
+  if (result != NO_ERROR)
+  {
+    showError(result);
+    delay(5000);
+    return;
+  }
+  
+  // print out the device's unique address
+  if (DEBUG)
+  {
+    Serial.print("addr=");
+    printHexBytes(addr, 8);
+    Serial.println();
+  }
+  
+  result = querySensor(addr, data);
+  if (result != NO_ERROR)
+  {
+    showError(result);
+    delay(5000);
+    return;
+  }
+  
+  // print out data from the scratchpad
+  if (DEBUG)
+  {
+    Serial.print("data=");
+    printHexBytes(data, 9);
+    Serial.println();
+  }
+  
+  result = validatePrecision(addr, data);
+  if (result != NO_ERROR)
+  {
+    showError(result);
+    delay(5000);
+    return;
+  }
+  
+  // first 2 bytes read are the temperature data, with the first
+  // byte being the least significant bits
+  sensorReading = (data[1] << 8) | data[0];
+  
+  showTemperature(sensorReading);
+  
+  delay(2000);
+}
+
+/**
+ * Looks for a single device on the 1-wire bus. If found, it's
+ * address will be written into addr and NO_ERROR will be
+ * returned, otherwise an error code will be returned.
+ */
+int findSingleDevice(byte *addr)
+{
+  // ensure we're looking for the first device on the 1-wire bus
+  ds.reset_search();
+  
+  // Search for the first device on the 1-wire bus. If found, its
   // unique 8-byte address will be written into addr and true is
   // returned, otherwise false is returned.
   if (!ds.search(addr))
   {
-    if (DEBUG)
-    {
-      Serial.println("No more addresses.");
-    }
-    
-    // if there are multiple devices on the 1-wire bus, this will
-    // reset so that the first device is returned on the next
-    // search 
-    ds.reset_search();
-    delay(1000);
-    return;
+    return ERROR_OWB_NO_DEVICES;
   }
   
-  // print out the next device's unique address
-  Serial.print("addr=");
-  for (i = 0; i < 8; ++i)
+  if (ds.search(addr))
   {
-    printHex(addr[i]);
-    Serial.print(' ');
+    return ERROR_OWB_TOO_MANY_DEVICES;
   }
- 
+  
   // verify valid address via CRC
   if (OneWire::crc8(addr, 7) != addr[7])
   {
-    Serial.println("CRC is not valid!");
-    return;
+    return ERROR_OWB_ADDRESS_CRC;
   }
   
-  if (addr[0] == 0x28)
+  if (addr[0] != 0x28)
   {
-    Serial.println("is a DS18B20 family device.");
-  }
-  else if (addr[0] == 0x10)
-  {
-    Serial.print("is a DS18S20 family device, only ");
-    Serial.println("DS18B20 family devices are supported.");
-    return;
-  }
-  else
-  {
-    Serial.print(", device family is not recognized: 0x");
-    Serial.println(addr[0], HEX);
-    return;
+    if (DEBUG)
+    {
+      Serial.print("ERROR: Only 0x28 device families are ");
+      Serial.print("supported, found 0x");
+      Serial.println(addr[0], HEX);
+    }
+    return ERROR_OWB_NOT_28_FAMILY;
   }
   
-  // reset state, select the discovered device on the 1-wire bus
+  return NO_ERROR;
+}
+
+int querySensor(byte *addr, byte *data)
+{
+  // all 1-wire bus command start with a reset
   ds.reset();
   ds.select(addr);
   
@@ -102,34 +157,63 @@ void loop(void)
   // on the DS18B20's Scratchpad memory)
   ds.write(0x44);
   
-  present = ds.reset();
+  ds.reset();
   ds.select(addr);
   
   ds.write(0xBE);
-  
-  Serial.print("data=");
-  for (i = 0; i < 9; ++i)
+
+  for (int i = 0; i < 9; ++i)
   {
     data[i] = ds.read();
-    printHex(data[i]);
-    Serial.print(' ');
   }
-  Serial.println();
   
   if (OneWire::crc8(data, 8) != data[8])
   {
-    Serial.println("CRC validation failed on data read!");
-    return;
+    return ERROR_OWB_SCRATCHPAD_READ_CRC;
   }
-  
-  Serial.println();
-  
-  sensorReading = (data[1] << 8) | data[0];
-  
+  return NO_ERROR;
+}
+
+int validatePrecision(byte *addr, byte *data)
+{
+  // TODO implement
+  return NO_ERROR;
+}
+
+void printHexBytes(byte *bytes, int len)
+{
+  for (int i = 0; i < len; ++i)
+  {
+    if (0 != i)
+    {
+      Serial.print(' ');
+    }
+    printHex(bytes[i]);
+  }
+}
+
+/**
+ * Prints a 2 digit hex value to the serial port.
+ */
+void printHex(byte b)
+{
+  // Serial.print(x, HEX), where 0 <= x <= F, only prints a single
+  // character, so pad it with a '0' (ie, print "04" instead of
+  // "4").
+  if (b < 0x10)
+  {
+    Serial.print('0');
+  }
+    
+  Serial.print(b, HEX);
+}
+
+void showTemperature(int sensorReading)
+{
   // On the DS18B20, the 12th through the 16th bit are the sign
   // bits. We'll only check the 16th bit to see if it's negative,
-  // ie, the binary number matches 1xxx xxxx xxxx xxxx
-  isNegative = (sensorReading & 0x8000) != 0;
+  // ie, the binary number matches 1xxx xxxx xxxx xxxx.
+  boolean isNegative = (sensorReading & 0x8000) != 0;
   
   if (isNegative)
   {
@@ -182,7 +266,7 @@ void loop(void)
 
   // To get the whole part of the temperature, divide by 16
   // (which is the same as bit shifting to the right by 4).
-  whole = sensorReading >> 4;
+  int wholeCelsius = sensorReading >> 4;
   
   // The fractional part of the temperature is represented by the
   // last 4 bits, so grab those (by masking with
@@ -201,30 +285,27 @@ void loop(void)
   // numbers (the 1's get pushed out)! To prevent this, we must
   // FIRST multiply by 100, THEN bit shift, ending up with 75 as
   // the "fractional part".
-  fract = ((sensorReading & 0x000F) * 100) >> 4;
+  int fractCelsius = ((sensorReading & 0x000F) * 100) >> 4;
   
   if (isNegative)
   {
     Serial.print('-');
   }
-  Serial.print(whole);
+  Serial.print(wholeCelsius);
   Serial.print('.');
-  Serial.print(fract);
+  Serial.print(fractCelsius);
   Serial.println(" C");
+  
+  // TODO show on 7-segment display
 }
 
-/**
- * Prints a 2 digit hex value to the serial port.
- */
-void printHex(byte b)
+void showError(int errorCode)
 {
-  // Serial.print(x, HEX), where 0 <= x <= F only prints a single
-  // character, so pad it with a '0' (ie, print "04" instead of
-  // "4").
-  if (b < 0x10)
+  if (DEBUG)
   {
-    Serial.print('0');
+    Serial.print("Error ");
+    Serial.println(errorCode);
   }
-    
-  Serial.print(b, HEX);
+  
+  // TODO show on 7-segment display
 }
